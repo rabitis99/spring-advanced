a575388229455fd51e7b3950ef5001f640385ec6
rabitis99 <kyungsu9914@gmail.com>
2025-04-17 15:21:24 +0900

    :adhesive_bandage: 레벨 1-2: 불필요한 if-else 피하기

--- src/main/java/org/example/expert/ExpertApplication.java
+++ src/main/java/org/example/expert/ExpertApplication.java
@@ -1,7 +1,7 @@
 package org.example.expert;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
