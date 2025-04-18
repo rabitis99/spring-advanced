d781142ca4b75bfad7df4e513f8a243d961f7964
rabitis99 <kyungsu9914@gmail.com>
2025-04-18 14:07:09 +0900

    :adhesive_bandage: 레벨 2: N+1 문제 fix param추가

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -21,7 +21,7 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN FETCH t.user " +
             "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(Long todoId);
+    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
 //    @EntityGraph(attributePaths = {"user"})
 //    Optional<Todo> findById(Long id);
