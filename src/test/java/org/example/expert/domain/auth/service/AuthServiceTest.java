87c86c8a4fe41c542ef13e6d31bef304070cf8ce
rabitis99 <kyungsu9914@gmail.com>
2025-04-18 15:13:07 +0900

    :ambulance: 레벨 3-2:manager->Todo

---
+++ src/test/java/org/example/expert/domain/auth/service/AuthServiceTest.java
@@ -0,0 +1,16 @@
+package org.example.expert.domain.auth.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceTest {
+
+    @Test
+    void signup() {
+    }
+
+    @Test
+    void signin() {
+    }
+}
