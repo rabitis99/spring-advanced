c6c4266e3c2f05638fa95bd04fdc0ae8662f383e
rabitis99 <kyungsu9914@gmail.com>
2025-04-19 16:46:58 +0900

    :zap: 레벨 5-3: feat filter_setOrder

--- src/main/java/org/example/expert/common/config/FilterConfig.java
+++ src/main/java/org/example/expert/common/config/FilterConfig.java
@@ -18,7 +18,7 @@ public class FilterConfig {
         FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new JwtFilter(jwtUtil));
         registrationBean.addUrlPatterns("/*"); // 필터를 적용할 URL 패턴을 지정합니다.
-
+        registrationBean.setOrder(1);
         return registrationBean;
     }
 }
