e211fa9684aec54d594ce500bcd490937d08b3c1
rabitis99 <kyungsu9914@gmail.com>
2025-04-19 16:44:31 +0900

    🚚 레벨 5-2: refactor @EnableJpaAuditing

--- src/main/java/org/example/expert/ExpertApplication.java
+++ src/main/java/org/example/expert/ExpertApplication.java
@@ -2,12 +2,13 @@ package org.example.expert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+@EnableJpaAuditing
 public class ExpertApplication {
 
     public static void main(String[] args) {
