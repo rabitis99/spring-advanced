acb9ca49830ff63650c6eaa5fa0dbd1d037c73f9
rabitis99 <kyungsu9914@gmail.com>
2025-04-20 11:28:49 +0900

    :zap:  레벨 5-5: feat httpStatus 값 표시 하도록 변ê경

--- src/main/java/org/example/expert/domain/auth/controller/AuthController.java
+++ src/main/java/org/example/expert/domain/auth/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.example.expert.domain.auth.dto.request.SignupRequest;
 import org.example.expert.domain.auth.dto.response.SigninResponse;
 import org.example.expert.domain.auth.dto.response.SignupResponse;
 import org.example.expert.domain.auth.service.AuthService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +19,12 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/signup")
-    public SignupResponse signup(@Valid @RequestBody SignupRequest signupRequest) {
-        return authService.signup(signupRequest);
+    public ResponseEntity<SignupResponse> signup(@Valid @RequestBody SignupRequest signupRequest) {
+        return ResponseEntity.ok().body(authService.signup(signupRequest));
     }
 
     @PostMapping("/auth/signin")
-    public SigninResponse signin(@Valid @RequestBody SigninRequest signinRequest) {
-        return authService.signin(signinRequest);
+    public ResponseEntity<SigninResponse> signin(@Valid @RequestBody SigninRequest signinRequest) {
+        return ResponseEntity.ok().body(authService.signin(signinRequest));
     }
 }
